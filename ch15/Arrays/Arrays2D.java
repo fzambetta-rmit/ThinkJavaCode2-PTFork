e38983ea729904f0eb57db1fc9fb97534825856c
Fabio Zambetta <fabio.zambetta@rmit.edu.au>
2020-04-29 15:53:26 +1000

    Added stand-alone example of 2D Arrays very basic usage.

---
+++ ch15/Arrays/Arrays2D.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+public class Arrays2D 
+{ 
+    private int[][] intTable = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }; 
+    private double[][] doubleTable = { { 1.1, 2.2, 3.3 }, { 4.4, 5.5, 6.6 }, { 7.7, 8.8, 9.9 } }; 
+    private String stringTable[][] = {{"Apple", "Banana"}, {"Cheesecake", "Black Forest", "Panettone"}};
+
+    private void printIntArray()
+    {
+        for (int i = 0; i < intTable.length; i++) 
+            for (int j = 0; j < intTable[i].length; j++) 
+                System.out.println("Table[" + i + "][" + j + "] = "
+                                   + intTable[i][j]); 
+    }
+
+    private void printDoubleArray()
+    {
+        for (int i = 0; i < doubleTable.length; i++) 
+            for (int j = 0; j < doubleTable[i].length; j++) 
+                System.out.println("Table[" + i + "][" + j + "] = "
+                                   + doubleTable[i][j]); 
+    }
+
+    private void printStringArray()
+    {
+        for (int i = 0; i < stringTable.length; i++) 
+            for (int j = 0; j < stringTable[i].length; j++) 
+                System.out.println("Table[" + i + "][" + j + "] = "
+                                   + stringTable[i][j]); 
+    }
+
+    public static void main(String[] args) 
+    { 
+        Arrays2D a2d = new Arrays2D();
+
+        a2d.printIntArray();
+        System.out.println("*******");
+        a2d.printDoubleArray();
+        System.out.println("*******");
+        a2d.printStringArray();
+    } 
+} 
